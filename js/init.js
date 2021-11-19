cd3085de3b436361c4887c958a92c98d1ef66322
CarmainDelta <fernando000bm@gmail.com>
2021-11-18 18:35:09 -0600

    Se agregaron archivos

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyAQspGxkrNuh0azZUXbZgNGz3OAjZAHYZE",
+  authDomain: "chat801fbm.firebaseapp.com",
+  projectId: "chat801fbm",
+  storageBucket: "chat801fbm.appspot.com",
+  messagingSenderId: "586314228506",
+  appId: "1:586314228506:web:ad7a03d303c33f4d4402a3",
+  measurementId: "G-5PSY8DG9E6"
+    });
